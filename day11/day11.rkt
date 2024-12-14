30d078c674157cff2340a09bf9610cfe7033997a
swishtail <92322890+swishtail@users.noreply.github.com>
2024-12-15 00:33:14 +0100

    Both parts

--- day11/day11.rkt
+++ day11/day11.rkt
@@ -42,3 +42,60 @@
 (display
  (part-one input))
 (newline)
+
+;;; Part Two
+(define (list->table l)
+  (if (null? l)
+      '()
+      (cons (cons (car l) 1)
+            (list->table (cdr l)))))
+
+(define (add-or-update-instance key value table)
+  (if (null? table)
+      (cons (cons key value)
+            table)
+      (if (equal? key (caar table))
+          (cons (cons key (+ (cdar table) value))
+                (cdr table))
+          (cons (car table)
+                (add-or-update-instance key value (cdr table))))))
+
+(define (apply-rule n m intermediate)
+  (let ((result
+         (if (zero? n)
+             1
+             (let ((num-digits (number-of-digits n)))
+               (if (even? num-digits)
+                   (split-in-two n num-digits)
+                   (* n 2024))))))
+    (if (list? result)
+        (add-or-update-instance
+         (car result) m (add-or-update-instance
+                         (cadr result) m intermediate))
+        (add-or-update-instance result m intermediate))))
+
+(define (traverse-list-with-rule stones)
+  (let iterate ((result '())
+                (remaining stones))
+    (if (null? remaining)
+        result
+        (iterate (apply-rule (caar remaining)
+                             (cdar remaining)
+                             result)
+                 (cdr remaining)))))
+
+(define (blink-many n stones)
+  (if (zero? n)
+      stones
+      (blink-many (- n 1)
+                  (traverse-list-with-rule stones))))
+
+(define (count-stones table)
+  (apply + (map cdr table)))
+
+(define (part-two x)
+  (count-stones (blink-many 75 (list->table x))))
+
+(display
+ (part-two input))
+(newline)
