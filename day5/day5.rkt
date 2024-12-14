88aec282ea108614fdb7af80199d111894f1927e
swishtail <92322890+swishtail@users.noreply.github.com>
2024-12-14 01:00:04 +0100

    Improved part 1, and now with part 2

--- day5/day5.rkt
+++ day5/day5.rkt
@@ -1,126 +1,131 @@
 #lang racket
 ;;; Advent of Code - Day 5: Print Queue
+
 (define input
   (file->lines "input"))
 
-(define (page-orders x)
-  (if (string=? "" (car x))
-      '()
-      (cons (car x)
-            (page-orders (cdr x)))))
-
-(define (page-numbers x)
-  (if (string=? "" (car x))
-      (cdr x)
-      (page-numbers (cdr x))))
+(define test-input
+  (file->lines "test-input"))
 
 (define (parse-page x)
   (map (λ (b) (map string->number b))
        (map (λ (a) (regexp-match* #rx"[0-9][0-9]" a))
             x)))
 
-(define input-page-orders
-  (parse-page (page-orders input)))
+(define (parse-input-page-numbers x)
+  (define (extract-page-numbers x)
+    (if (string=? "" (car x))
+        (cdr x)
+        (extract-page-numbers (cdr x))))
+  (parse-page (extract-page-numbers x)))
 
-(define input-page-numbers
-  (parse-page (page-numbers input)))
-
-;;; Test input
-(define test-input
-  '("47|53"
-    "97|13"
-    "97|61"
-    "97|47"
-    "75|29"
-    "61|13"
-    "75|53"
-    "29|13"
-    "97|29"
-    "53|29"
-    "61|53"
-    "97|53"
-    "61|29"
-    "47|13"
-    "75|47"
-    "97|75"
-    "47|61"
-    "75|61"
-    "47|29"
-    "75|13"
-    "53|13"
-    ""
-    "75,47,61,53,29"
-    "97,61,53,29,13"
-    "75,29,13"
-    "75,97,47,61,53"
-    "61,13,29"
-    "97,13,75,29,47"))
-
-(define test-input-page-orders
-  (parse-page (page-orders test-input)))
-
-(define test-input-page-numbers
-  (parse-page (page-numbers test-input)))
-
-(define true-test
-  (list-ref test-input-page-numbers 0))
-
-(define false-test
-  (list-ref test-input-page-numbers 3))
+(define (parse-input-page-orders x)
+  (define (extract-page-orders x)
+    (if (string=? "" (car x))
+        '()
+        (cons (car x)
+              (extract-page-orders (cdr x)))))
+  (parse-page (extract-page-orders x)))
 
 ;;; Part One
-(define (correctly-ordered-updates page-numbers page-orders)
-  (define (special-assoc* key assoc-list)  ; This version of assoc* returns an
-    (if (assoc key assoc-list)             ; empty list if no instances of key
-        (let loop ((remaining assoc-list)) ; are found
-          (if (null? remaining)
-              '()
-              (if (equal? key (caar remaining))
-                  (cons (car remaining)
-                        (loop (cdr remaining)))
-                  (loop (cdr remaining)))))
-        '()))
-  (define (validate-list-of-page-numbers list-of-page-numbers
-                                         list-of-page-orders)
-    (define (test-first-number l list-of-page-orders)
-      (define (list-second-element-of-sublists assoc-list)
-        (if (null? assoc-list)
+(define (assoc* key assoc-list)
+  (if (assoc key assoc-list)
+      (let loop ((remaining assoc-list))
+        (if (null? remaining)
             '()
-            (cons (cadar assoc-list)
-                  (list-second-element-of-sublists (cdr assoc-list)))))
-      (let* ((list-of-possible-next-pages
-              (list-second-element-of-sublists (special-assoc*
-                                                (car l)
-                                                list-of-page-orders))))
-        (let loop ((remaining (cdr l)))
-          (if (null? remaining)
-              #t
-              (if (member (car remaining)
-                          list-of-possible-next-pages)
-                  (loop (cdr remaining))
-                  #f)))))
-    (if (null? list-of-page-numbers)
-        #t
-        (if (test-first-number list-of-page-numbers list-of-page-orders)
-            (validate-list-of-page-numbers (cdr list-of-page-numbers)
-                                           list-of-page-orders)
-            #f)))
-  (filter (lambda (a) (validate-list-of-page-numbers a page-orders))
-          page-numbers))
+            (if (equal? key (caar remaining))
+                (cons (car remaining)
+                      (loop (cdr remaining)))
+                (loop (cdr remaining)))))
+      '()))
 
-(define (middle-of-page-numbers list-of-page-numbers)
-  (list-ref list-of-page-numbers
-            (floor (/ (length list-of-page-numbers)
+(define (search value assoc-list)
+  (if (null? assoc-list)
+      #f
+      (if (equal? value (cadar assoc-list))
+          (car assoc-list)
+          (search value (cdr assoc-list)))))
+
+(define (legal-page-order? page-list page-orders)
+  (define (all-false? lb)
+    (if (null? lb)
+        #t
+        (if (car lb)
+            #f
+            (all-false? (cdr lb)))))
+  (define (test-car page-list)
+    (let* ((first (car page-list))
+           (remaining (cdr page-list))
+           (assoc-lists-of-remaining
+            (map (λ (a) (assoc* a page-orders))
+                 remaining)))
+      (if (all-false?
+           (map (λ (a) (search first a))
+                assoc-lists-of-remaining))
+          #t
+          #f)))
+  (let iterate ((remaining page-list))
+    (if (null? remaining)
+        #t
+        (if (test-car remaining)
+            (iterate (cdr remaining))
+            #f))))
+
+(define (middle-of-page-list page-list)
+  (list-ref page-list
+            (floor (/ (length page-list)
                       2))))
+
+(define (correctly-ordered-page-lists page-numbers page-orders)
+  (filter (λ (a) (legal-page-order? a page-orders))
+          page-numbers))
 
 (define (part-one page-numbers page-orders)
   (foldl +
          0
-         (map middle-of-page-numbers
-              (correctly-ordered-updates page-numbers
-                                         page-orders))))
+         (map middle-of-page-list
+              (correctly-ordered-page-lists page-numbers
+                                            page-orders))))
 
 (display
- (part-one input-page-numbers
-           input-page-orders))
+ (part-one (parse-input-page-numbers input)
+           (parse-input-page-orders input)))
 (newline)
+
+;;; Part Two
+(define (incorrectly-ordered-page-lists page-numbers page-orders)
+  (filter (λ (a) (not (legal-page-order? a page-orders)))
+          page-numbers))
+
+(define (correct-page-list page-list page-orders)
+  (let test-list ((current-list page-list))
+    (if (legal-page-order? current-list
+                           page-orders)
+        current-list
+        (test-list
+         (let recurse-inner ((remaining current-list))
+           (if (null? (cdr remaining))
+               remaining
+               (if (legal-page-order? (take remaining 2)
+                                      page-orders)
+                   (cons (car remaining)
+                         (recurse-inner (cdr remaining)))
+                   (let ((first (car remaining))
+                         (second (cadr remaining)))
+                     (cons second
+                           (recurse-inner (cons first
+                                                (cddr remaining))))))))))))
+
+(define (part-two incorrect-page-numbers page-orders)
+  (foldl +
+         0
+         (map middle-of-page-list
+              (map (λ (a) (correct-page-list a page-orders))
+                   incorrect-page-numbers))))
+
+(display
+ (let ((page-orders (parse-input-page-orders input)))
+   (part-two (incorrectly-ordered-page-lists
+              (parse-input-page-numbers input)
+              page-orders)
+             page-orders)))
